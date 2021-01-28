dc92aa9078511ad7b1e953338ad907b263c48157
ssc <ssc@163.com>
2021-01-28 14:00:37 +0800

    第二次提交文件

---
+++ src/main/java/SecondFile.java
@@ -0,0 +1,5 @@
+public class SecondFile {
+    public static void main(String[] args) {
+        System.out.println("这是第二个文件");
+    }
+}
--- src/main/java/TestFile.java
+++ src/main/java/TestFile.java
@@ -1,5 +1,5 @@
 public class TestFile {
     public static void main(String[] args) {
-        System.out.println("haha");
+        System.out.println("这是修改过的第一个文件");
     }
 }
